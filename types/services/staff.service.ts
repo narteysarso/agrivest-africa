d79fe1935e3c3c15ff4f65f41f992576f988dd13
narteysarso <narteysarso@gmail.com>
2024-08-27 12:07:21 +0000

    fix build error

--- types/services/staff.service.ts
+++ types/services/staff.service.ts
@@ -1,13 +1,13 @@
-import { UserInput } from '..';
+import { StaffInput } from '..';
 
-export interface IUserService {
-    createUser(input: UserInput): Promise<any>;
-    findUserByEmail(): Promise<any>;
-    findUserById(): Promise<any>;
-    findUser(input: Partial<UserInput>, options: any): Promise<any>;
-    updateUser(): Promise<any>;
-    updateUserPassword(): Promise<any>;
-    markUserDeleted(): Promise<any>;
-    deleteAllUsers(): Promise<any>;
-    loginUser(input: Partial<UserInput>): Promise<any>;
+export interface IStaffService {
+    createStaff(input: StaffInput): Promise<any>;
+    findStaffByEmail(): Promise<any>;
+    findStaffById(): Promise<any>;
+    findStaff(input: Partial<StaffInput>, options: any): Promise<any>;
+    updateStaff(): Promise<any>;
+    updateStaffPassword(): Promise<any>;
+    markStaffDeleted(): Promise<any>;
+    deleteAllStaffs(): Promise<any>;
+    loginStaff(input: Partial<StaffInput>): Promise<any>;
 }
